7d4fb007d136232b677706a80fb23d5d151be6d8
Menno van Hout <info@mennovanhout.nl>
2025-05-15 11:47:11 +0200

    Add cors

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Http\Middleware\HandleCors;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,7 +15,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->append(HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
--- config/cors.php
+++ config/cors.php
@@ -1,11 +1,34 @@
 <?php
+
 return [
-    'paths' => ['*'], // Apply CORS to all routes
-    'allowed_methods' => ['*'], // Allow all HTTP methods
-    'allowed_origins' => ['*'], // Allow all origins
-    'allowed_origins_patterns' => [], // No regex patterns
-    'allowed_headers' => ['*'], // Allow all headers
-    'exposed_headers' => [], // No custom headers exposed
-    'max_age' => 0, // Cache duration for preflight requests
-    'supports_credentials' => false, // Disable credentials with wildcard origins
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => ['*'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => false,
+
 ];

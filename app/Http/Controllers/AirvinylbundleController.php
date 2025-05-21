69d56008411105e2799c85c3b8a53b407a1e2e38
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-05-21 20:01:38 +0200

    Create AirvinylbundleController.php
    new bundle

---
+++ app/Http/Controllers/AirvinylbundleController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ProductRequest;
+use Illuminate\Support\Str;
+use Signifly\Shopify\Shopify;
+
+class AirvinylbundleController extends Controller
+{
+    public function store(ProductRequest $request)
+    {
+        $data = $request->validated();
+
+        $title = $data['title'];
+        $handle = Str::slug($title);
+        $image = 'https://dtchdesign.nl/create-product/imgbundle.php?albumImg=' . urlencode($data['images'][0] ?? '');
+
+        $shopify = new Shopify(
+            config('albumtagz.shop_access_code'),
+            config('albumtagz.shop_url'),
+            config('albumtagz.shop_api_version')
+        );
+
+        $bodyHtml = "<p>Bundle includes:</p><ul>";
+        foreach ($data['artists'] as $index => $artist) {
+            $url = $data['spotifyUrls'][$index] ?? '#';
+            $bodyHtml .= "<li>{$artist} - <a href='{$url}' target='_blank'>{$url}</a></li>";
+        }
+        $bodyHtml .= "</ul>";
+
+        $product = $shopify->createProduct([
+            'title' => "{$title} Refill Bundle",
+            'vendor' => 'AlbumTagz Bundle',
+            'product_type' => 'Music Bundle',
+            'status' => 'active',
+            'handle' => $handle . '-bundle',
+            'template_suffix' => 'airvinyl-customize',
+            'body_html' => $bodyHtml,
+            'variants' => [
+                [
+                    'price' => "14.95",
+                    'compare_at_price' => "19.95",
+                    'requires_shipping' => true,
+                    'inventory_management' => null,
+                ]
+            ],
+            'images' => array_map(function ($img, $i) use ($handle) {
+                return [
+                    'src' => 'https://dtchdesign.nl/create-product/imgrefill.php?albumImg=' . urlencode($img),
+                    'filename' => 'bundle_' . $i . '.jpg'
+                ];
+            }, $data['images'], array_keys($data['images']))
+        ]);
+
+        return response()->json([
+            'shopify_url' => 'https://www.albumtagz.com/products/' . $product['handle']
+        ]);
+    }
+}

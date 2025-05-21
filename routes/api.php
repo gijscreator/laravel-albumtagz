fe547cb031432e0f59c42cb7141013741f1ecf65
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-05-21 07:49:58 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\SearchController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test', function () {
+    return response()->json(['message' => 'It works']);
+});
+
+
 Route::get('/search', [SearchController::class, 'search']);
 Route::post('/products', [ProductsController::class, 'store']);
 Route::post('/products/keep', [ProductsController::class, 'keep']);

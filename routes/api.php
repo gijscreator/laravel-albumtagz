0676047fdb60eff238308c77c6c9197f6640444d
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-05-21 07:58:32 +0200

    Update api.php
    added extra files for product

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,10 @@
 use App\Http\Controllers\ProductsControllerAlbumtagz;
 use App\Http\Controllers\ProductsControllerAirvinyls;
 use App\Http\Controllers\ProductsControllerAirvinylrefill;
+use App\Http\Controllers\SearchController;
 
 Route::post('/products/albumtagz', [ProductsControllerAlbumtagz::class, 'store']);
 Route::post('/products/airvinyls', [ProductsControllerAirvinyls::class, 'store']);
 Route::post('/products/airvinylrefill', [ProductsControllerAirvinylrefill::class, 'store']);
+
+Route::get('/search', [SearchController::class, 'search']);

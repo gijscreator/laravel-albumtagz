d4c9dc4a917b85a588b0475decc167f7d632e0fd
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-05-21 20:24:07 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,13 +3,11 @@
 use App\Http\Controllers\ProductsController;
 use App\Http\Controllers\productsControllerAirvinylrefill;
 use App\Http\Controllers\ProductsControllerAirvinyls;
-use App\Http\Controllers\AirvinylbundleController;
 use App\Http\Controllers\SearchController;
 
 Route::post('/products/albumtagz', [ProductsController::class, 'store']);
 Route::post('/products/airvinyls', [ProductsControllerAirvinyls::class, 'store']);
 Route::post('/products/airvinylrefill', [ProductsControllerAirvinylrefill::class, 'store']);
-Route::post('/products/airvinylbundle', [AirvinylbundleController::class, 'store']);
 Route::post('/products/keep', [ProductsController::class, 'keep']);
 Route::get('/search', [SearchController::class, 'search']);
 

ddb6e5e21874fff7177ed2dfa4302a553bd6adea
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-05-21 08:33:15 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => [
+    'https://www.albumtagz.com',
+    'https://albumtagz.com', // indien zonder www ook gebruikt
+     ],
+
 
     'allowed_origins_patterns' => [],
 

fe5cc1dc1a4d0e3ad861ebe1d21a851f71ed4e7f
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-06-10 12:53:00 +0200

    Update ProductsController.php

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -57,26 +57,27 @@ class ProductsController extends Controller
                     ]
                 ],
 'images' => [
-    [ // Upload this dynamic image
+    [
         'src' => $image,
         'filename' => 'mockup_' . $handle . '.jpg'
     ],
-    [ // Just link to already uploaded static images
+    [
         'src' => 'https://cdn.shopify.com/s/files/1/0879/3322/3247/files/12_3.jpg?v=1749543016',
+        'filename' => 'extra_' . $handle . '.jpg'
     ],
     [
         'src' => 'https://cdn.shopify.com/s/files/1/0879/3322/3247/files/copy_3C9E333D-9F93-4B1D-8CA5-3D29FD978A62.gif?v=1738171019',
+        'filename' => 'copy_' . $handle . '.gif'
     ],
     [
         'src' => 'https://cdn.shopify.com/s/files/1/0879/3322/3247/files/image.gif?v=1736846951',
+        'filename' => 'image_' . $handle . '.gif'
     ]
 ]
 
 
-
             ]
         );
-
         // Create it in our database
         $album = Album::create([
             'shopify_id' => $product['id'],

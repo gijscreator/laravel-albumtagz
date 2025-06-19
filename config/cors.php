38277c1bc61f2de78fa1ffcf169bd41cd09ed562
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-06-19 12:52:52 +0200

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -22,7 +22,7 @@ return [
     'allowed_origins' => [
     'https://www.albumtagz.com',
     'https://albumtagz.com',
-    'https://*.lovable.app',
+    'https://a4f650a3-30e8-43ad-a83a-3f4c1e622c95.lovableproject.com',
     'http://127.0.0.1:3002' // indien zonder www ook gebruikt
      ],
 

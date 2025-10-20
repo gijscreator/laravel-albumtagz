6dbf371c054288dd26efe495a3141fa6012c9552
Gijs <65440619+gijscreator@users.noreply.github.com>
2025-10-20 13:20:53 +0200

    Add route for storing keychains
    Added a new route for storing keychains in the ProductsController.

--- routes/api.php
+++ routes/api.php
@@ -10,6 +10,8 @@ Route::post('/products/airvinyls', [ProductsControllerAirvinyls::class, 'store']
 Route::post('/products/airvinylrefill', [ProductsControllerAirvinylrefill::class, 'store']);
 Route::post('/products/keep', [ProductsController::class, 'keep']);
 Route::get('/search', [SearchController::class, 'search']);
+Route::post('/products/keychains', [ProductsController::class, 'storeKeychain']);
+
 
 // 🆕 New custom keychain route
 Route::post('/products/keychains', [ProductsController::class, 'storeKeychain']);

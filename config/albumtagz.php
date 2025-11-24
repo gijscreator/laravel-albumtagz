c2bfa17091ac1b20f0488e85fea23ae70f4c9b3b
gijscreator <65440619+gijscreator@users.noreply.github.com>
2025-11-24 17:48:19 +0100

    new revert

--- config/albumtagz.php
+++ config/albumtagz.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'api_key' => env('ALBUMTAGZ_API_KEY', '2dce51bf45bfe666ea11b681d68dbc41'),
-    'shop_url' => env('ALBUMTAGZ_SHOP_URL', 'qu2e81-ss.myshopify.com'),
-    'shop_access_code' => env('ALBUMTAGZ_SHOP_ACCESS_CODE', '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'),
-    'shop_api_version' => env('ALBUMTAGZ_SHOP_API_VERSION', '2025-10'),
+    'api_key' => env('ALBUMTAGZ_API_KEY', 'd83bbff8e28feb236151937ca02a69d9'),
+    'shop_url' => env('ALBUMTAGZ_SHOP_URL', '745b68-a1.myshopify.com'),
+    'shop_access_code' => env('ALBUMTAGZ_SHOP_ACCESS_CODE', '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'),
+    'shop_api_version' => env('ALBUMTAGZ_SHOP_API_VERSION', '2025-01'),
 ];
